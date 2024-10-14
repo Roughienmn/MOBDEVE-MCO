07329a666fd96b239caa388d224b3425ab8fb599
Aldrich Go <aldrichgo3600@gmail.com>
2024-10-14 15:39:42 +0800

    Made it start with the loader then it goes to splashscreenactivity

---
+++ YummyFood4Lyfe/app/src/main/java/com/example/yummyfood4lyfe/LoaderActivity.java
@@ -0,0 +1,24 @@
+package com.example.yummyfood4lyfe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoaderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loader);
+
+        // Delay for 2 seconds and then start SplashScreenActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoaderActivity.this, SplashScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000); // 2000 milliseconds = 2 seconds
+    }
+}
---
+++ YummyFood4Lyfe/app/src/main/java/com/example/yummyfood4lyfe/SplashScreenActivity.java
@@ -0,0 +1,4 @@
+package com.example.yummyfood4lyfe;
+
+public class SplashScreenActivity {
+}
