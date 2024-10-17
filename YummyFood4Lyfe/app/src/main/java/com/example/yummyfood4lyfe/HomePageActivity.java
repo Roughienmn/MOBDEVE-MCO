ef7aa156762b4f9de39dc293846e559dfc86a369
Aldrich Go <aldrichgo3600@gmail.com>
2024-10-17 10:44:29 +0800

    added recyclerview to homepage

--- YummyFood4Lyfe/app/src/main/java/com/example/yummyfood4lyfe/HomePageActivity.java
+++ YummyFood4Lyfe/app/src/main/java/com/example/yummyfood4lyfe/HomePageActivity.java
@@ -2,11 +2,22 @@ package com.example.yummyfood4lyfe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.Arrays;
+import java.util.List;
 
 public class HomePageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        RecyclerView recyclerView = findViewById(R.id.rcview_recommended);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<String> dataList = Arrays.asList("Classic Chicken Adobo", "Another Recipe", "More Recipes");
+        RecommendedListAdapter adapter = new RecommendedListAdapter(dataList);
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ YummyFood4Lyfe/app/src/main/java/com/example/yummyfood4lyfe/RecommendedListAdapter.java
@@ -0,0 +1,45 @@
+package com.example.yummyfood4lyfe;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class RecommendedListAdapter extends RecyclerView.Adapter<RecommendedListAdapter.ViewHolder> {
+
+    private List<String> dataList;
+
+    public RecommendedListAdapter(List<String> dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recommended_list, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String data = dataList.get(position);
+        holder.textView.setText(data);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView20); // Adjust this ID based on your layout
+        }
+    }
+}
